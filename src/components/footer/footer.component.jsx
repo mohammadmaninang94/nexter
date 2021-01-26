3ba06b5f5e0638e6cfa0ae5a0d3de8d95c37ec81
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-01-26 10:06:18 +0800

    added footer component and styled with flexbox

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,7 +1,19 @@
 import './footer.styles.scss';
 
 const Footer = () => (
-    <footer className='footer'>Footer</footer>
+    <footer className='footer'>
+        <ul className='nav'>
+            <li className='nav__item'><a href='#link' className='nav__link'>find your dream house</a></li>
+            <li className='nav__item'><a href='#link' className='nav__link'>request proposal</a></li>
+            <li className='nav__item'><a href='#link' className='nav__link'>download home planner</a></li>
+            <li className='nav__item'><a href='#link' className='nav__link'>contact us</a></li>
+            <li className='nav__item'><a href='#link' className='nav__link'>submit your property</a></li>
+            <li className='nav__item'><a href='#link' className='nav__link'>come work with us!</a></li>
+        </ul>
+        <p className="copyright">
+            &copy; Copyright 2021 by Mohammad Maninang.
+        </p>
+    </footer>
 );
 
 export default Footer;
--- src/components/section-gallery/section-gallery.component.jsx
+++ src/components/section-gallery/section-gallery.component.jsx
@@ -2,46 +2,46 @@ import './section-gallery.styles.scss';
 
 const SectionGallery = () => (
     <section className='gallery'>
-        <figure class="gallery__item gallery__item--1">
+        <figure className="gallery__item gallery__item--1">
             <img src="img/gal-1.jpeg" alt="Gallery house 1" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--2">
+        <figure className="gallery__item gallery__item--2">
             <img src="img/gal-2.jpeg" alt="Gallery house 2" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--3">
+        <figure className="gallery__item gallery__item--3">
             <img src="img/gal-3.jpeg" alt="Gallery house 3" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--4">
+        <figure className="gallery__item gallery__item--4">
             <img src="img/gal-4.jpeg" alt="Gallery house 4" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--5">
+        <figure className="gallery__item gallery__item--5">
             <img src="img/gal-5.jpeg" alt="Gallery house 5" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--6">
+        <figure className="gallery__item gallery__item--6">
             <img src="img/gal-6.jpeg" alt="Gallery house 6" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--7">
+        <figure className="gallery__item gallery__item--7">
             <img src="img/gal-7.jpeg" alt="Gallery house 7" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--8">
+        <figure className="gallery__item gallery__item--8">
             <img src="img/gal-8.jpeg" alt="Gallery house 8" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--9">
+        <figure className="gallery__item gallery__item--9">
             <img src="img/gal-9.jpeg" alt="Gallery house 9" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--10">
+        <figure className="gallery__item gallery__item--10">
             <img src="img/gal-10.jpeg" alt="Gallery house 10" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--11">
+        <figure className="gallery__item gallery__item--11">
             <img src="img/gal-11.jpeg" alt="Gallery house 11" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--12">
+        <figure className="gallery__item gallery__item--12">
             <img src="img/gal-12.jpeg" alt="Gallery house 12" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--13">
+        <figure className="gallery__item gallery__item--13">
             <img src="img/gal-13.jpeg" alt="Gallery house 13" className="gallery__img" />
         </figure>
-        <figure class="gallery__item gallery__item--14">
+        <figure className="gallery__item gallery__item--14">
             <img src="img/gal-14.jpeg" alt="Gallery house 14" className="gallery__img" />
         </figure>
     </section>
