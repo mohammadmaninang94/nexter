05c81f3a60d7eb28a5fb3a5c8fbff3d30e499e75
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-01-27 09:54:33 +0800

    added sidebar and realtors component

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,7 +1,9 @@
 import './sidebar.styles.scss';
 
 const SideBar = () => (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+        <button type='button' className='sidebar__nav-btn'></button>
+    </div>
 );
 
 export default SideBar;
