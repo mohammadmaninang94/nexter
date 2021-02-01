aee14840472934a5cbaf8754617dc2b63b547c65
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-02-01 09:59:09 +0800

    chnage flexbox to grid css in header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,20 +3,16 @@ import './header.styles.scss';
 const Header = () => (
     <header className='header'>
         <img src='img/logo.png' alt='Nexter Logo' className='header__logo' />
-        <div className='header__title-box'>
-            <h3 className='heading-3'>Your own home:</h3>
-            <h1 className='heading-1 mb-sm'>The ultimate personal freedom</h1>
-            <button type='button' className='btn header__btn'>View our properties</button>
-        </div>
-        <div className='header__seenon-box'>
-            <div className='header__seenon-text'>Seen on</div>
-            <figure className='header__seenon-list'>
-                <img src='img/logo-bbc.png' alt='Logo BBC' className='header__seenon-img' />
-                <img src='img/logo-forbes.png' alt='Logo Forbes' className='header__seenon-img' />
-                <img src='img/logo-techcrunch.png' alt='Logo TechCrunch' className='header__seenon-img' />
-                <img src='img/logo-bi.png' alt='Logo Business Insider' className='header__seenon-img' />
-            </figure>
-        </div>
+        <h3 className='heading-3'>Your own home:</h3>
+        <h1 className='heading-1 mb-sm'>The ultimate personal freedom</h1>
+        <button type='button' className='btn header__btn'>View our properties</button>
+        <div className='header__seenon-text mb-sm'>Seen on</div>
+        <figure className='header__seenon-list'>
+            <img src='img/logo-bbc.png' alt='Logo BBC' className='header__seenon-img' />
+            <img src='img/logo-forbes.png' alt='Logo Forbes' className='header__seenon-img' />
+            <img src='img/logo-techcrunch.png' alt='Logo TechCrunch' className='header__seenon-img' />
+            <img src='img/logo-bi.png' alt='Logo Business Insider' className='header__seenon-img' />
+        </figure>
     </header>
 );
 
