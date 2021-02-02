9de175c7e254851fcf2cdb5b3c8165432f111b07
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-02-02 09:10:08 +0800

    added media query for header and sidebar components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,9 @@ const Header = () => (
     <header className='header'>
         <img src='img/logo.png' alt='Nexter Logo' className='header__logo' />
         <h3 className='heading-3'>Your own home:</h3>
-        <h1 className='heading-1 mb-sm'>The ultimate personal freedom</h1>
+        <h1 className='heading-1'>The ultimate personal freedom</h1>
         <button type='button' className='btn header__btn'>View our properties</button>
-        <div className='header__seenon-text mb-sm'>Seen on</div>
+        <div className='header__seenon-text'>Seen on</div>
         <figure className='header__seenon-list'>
             <img src='img/logo-bbc.png' alt='Logo BBC' className='header__seenon-img' />
             <img src='img/logo-forbes.png' alt='Logo Forbes' className='header__seenon-img' />
